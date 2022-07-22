a0134963b9b2cc4aa0cfce580f3153d33d5e6f9a
Lupiac <antoine.lupiac0@gmail.com>
2022-07-22 16:53:23 +0200

    [REACT-UI] Refactored code + styled tab bar

---
+++ react-ui/src/components/TabBar/TabBar.tsx
@@ -0,0 +1,55 @@
+import { IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs } from '@ionic/react';
+import { cog, nutrition, search, time } from 'ionicons/icons';
+import { Redirect, Route } from 'react-router-dom';
+import HistoryTab from '../../pages/HistoryTab/HistoryTab';
+import PlantListTab from '../../pages/PlantListTab/PlantListTab';
+import SearchTab from '../../pages/SearchTab/SearchTab';
+import SettingsTab from '../../pages/SettingsTab/SettingsTab';
+
+const TabBar: React.FC = () => {
+  return (
+    <IonTabs>
+        <IonRouterOutlet>
+          <Route exact path="/plantListTab">
+            <PlantListTab/>
+          </Route>
+          <Route exact path="/searchTab">
+            <SearchTab/>
+          </Route>
+          <Route path="/historyTab">
+            <HistoryTab/>
+          </Route>
+          <Route path="/settingsTab">
+            <SettingsTab/>
+          </Route>
+          <Route exact path="/">
+            <Redirect to="/plantListTab"/>
+          </Route>
+        </IonRouterOutlet>
+        <IonTabBar slot="bottom">
+          <IonTabButton tab="plantListTab" href="/plantListTab">
+            <IonIcon icon={nutrition}/>
+            <IonLabel>Aliments</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="searchTab" href="/searchTab">
+            <div className='search-tab'>
+                <IonIcon icon={search}/>
+                <IonLabel>Recherche</IonLabel>
+            </div>
+          </IonTabButton>
+          <IonTabButton tab="historyTab" href="/historyTab">
+            <div className='search-tab'>
+                <IonIcon icon={time}/>
+                <IonLabel>Historique</IonLabel>
+            </div>
+          </IonTabButton>
+          <IonTabButton tab="settingsTab" href="/settingsTab">
+            <IonIcon icon={cog}/>
+            <IonLabel>Réglages</IonLabel>
+          </IonTabButton>
+        </IonTabBar>
+      </IonTabs>
+  );
+};
+
+export default TabBar;
