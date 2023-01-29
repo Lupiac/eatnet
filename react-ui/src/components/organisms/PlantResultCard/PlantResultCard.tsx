baed7448faff068f8fb9323983ed65a1760e04a5
Lupiac <antoine.lupiac0@gmail.com>
2023-01-29 12:40:54 +0100

    [REACT UI] Added plantnet informations to plant result cards

--- react-ui/src/components/organisms/PlantResultCard/PlantResultCard.tsx
+++ react-ui/src/components/organisms/PlantResultCard/PlantResultCard.tsx
@@ -2,7 +2,12 @@ import { IonAccordion, IonAccordionGroup, IonIcon, IonImg, IonItem, IonLabel, Io
 import { arrowDownCircle } from "ionicons/icons";
 import Plant from "../../../models/plant";
 import PlantToxicities from "../../../models/plantToxicities";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, EffectCreative} from 'swiper';
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-creative';
 function PlantResultCard(props: any){
   const {plant}: {plant:Plant} = props;
   const getToxicityClass = (value: number) =>{
@@ -11,9 +16,58 @@ function PlantResultCard(props: any){
   return (
     <article className="plant-result-card">
         <header>
-            <IonImg src={plant.url} />
+            <Swiper 
+              slidesPerView={1.1}
+              modules={[Pagination, EffectCreative]}
+              pagination={true}
+              centeredSlides={true}
+              effect={'creative'} 
+              creativeEffect={{
+                "perspective": true,
+                "prev": {
+                  "shadow": false,
+                  "translate": [
+                    "-50%",
+                    0,
+                    0
+                  ],
+                  "scale": 0.75
+                },
+                "next": {  
+                  "shadow": false,
+                  "translate": [
+                    "50%",
+                    0,
+                    0
+                  ],
+                  "scale": 0.75
+                }
+              }}
+            >
+              {Object.keys(plant.images).length===0?
+                  <>
+                  <SwiperSlide>
+                      <IonImg key={plant.url} src={plant.url} />
+                  </SwiperSlide>
+                  </>
+              :
+              Object.keys(plant.images).map((image: any)=>(
+                <>
+                <SwiperSlide key={image}>
+                    <IonImg className={`ico-${image}`} src={plant.images[image]} />
+                </SwiperSlide>
+                </>
+              ))}
+              {}
+            </Swiper>
             <div className="common-name">{plant.name}</div>
             <div className="scientific-name">{plant.scientificName}</div>
+            <div className="scientific-name">Plantnet: {plant.plantNetScientificName}</div>
+            <ul className="common-names">
+              {plant.commonNames.map((name:any, nameIndex:any)=>(
+                <li key={nameIndex}>{name}</li>
+              ))}
+            </ul>
         </header>
         <ul className="edible-tags">
           {Object.keys(plant.toxicities).map( (toxicity: string) =>{
